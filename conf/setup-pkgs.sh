59e15ae042462cf21cdf410de21654b6dff0f8fd
michaelilgiaev <michaelilgiaev@gmail.com>
2025-06-04 05:17:44 +0300

    Add pinta

--- conf/setup-pkgs.sh
+++ conf/setup-pkgs.sh
@@ -6,6 +6,7 @@ sudo ufw default allow outgoing
 sudo chmod +x /usr/bin/yay
 sudo chmod +x /usr/bin/neofetch
 sudo chmod +x /usr/bin/rar
+sudo chmod +x /usr/bin/pinta
 sudo mkdir -p /var/lib/pacman/local
 sudo cp -r /root/temp/var/lib/pacman/local/* /var/lib/pacman/local/
 sudo rm -rf /root/temp
