8169b0b5abd5a0f1e73c37959a16a4637e5f3e7a
michaelilgiaev <michaelilgiaev@gmail.com>
2025-07-11 18:31:32 +0300

    Slightly improve 'easy-arch-packages-cache.sh'

--- conf/finalize/easy-arch-packages-cache.sh
+++ conf/finalize/easy-arch-packages-cache.sh
@@ -9,48 +9,48 @@ fail() {
 }
 
 # Set temp location for pacman to safely write
-TMPDB=/tmp/pacstrap-easyarch-db
-MNTREPO=/mnt/pacstrap-easyarch-repo
+DB=/tmp/pacman-easyarch-db
+REPO=/mnt/pacman-easyarch-repo
 
 # Set final destination (relative to current dir)
-FINALDB=airootfs/root/pacstrap-easyarch-db
-FINALCACHE=airootfs/root/pacstrap-easyarch-repo
+FINALDB=easy-arch-packages-cache/pacman-easyarch-db
+FINALCACHE=easy-arch-packages-cache/pacman-easyarch-repo
 
 # Create safe temp dirs
-mkdir -p $TMPDB/sync
-mkdir -p $MNTREPO
+mkdir -p $DB/sync
+mkdir -p $REPO
 
 echo "[*] Downloading and caching base packages using pacman..."
 
 # Important: Initialize pacman database for custom dbpath
 echo "[*] Initializing pacman database in temporary path..."
-sudo pacman -Sy --dbpath $TMPDB --cachedir $MNTREPO --noconfirm
+sudo pacman -Sy --dbpath $DB --cachedir $REPO --noconfirm
 
 echo "[*] Preparing package list..."
-pkgs=$(tr '\n' ' ' < packages.x86_64)
+pkgs=$(jq -r '.packages[]' easy-arch-configuration.json | tr '\n' ' ')
 
 echo "[*] Downloading and caching each package individually (auto-resolving dependencies)..."
 
 for pkg in $pkgs; do
     echo "    [+] Downloading: $pkg"
-    sudo pacman -Sw --noconfirm --cachedir $MNTREPO --dbpath $TMPDB $pkg || fail "$pkg"
+    sudo pacman -Sw --noconfirm --cachedir $REPO --dbpath $DB $pkg || fail "$pkg"
 done
 
 echo "[*] Creating local repository..."
-repo-add $MNTREPO/pacstrap-easyarch-repo.db.tar.gz $MNTREPO/*.tar.zst || fail "repo-add"
+repo-add $REPO/pacman-easyarch-repo.db.tar.gz $REPO/*.tar.zst || fail "repo-add"
 
 # Create final destination dirs
 mkdir -p $FINALDB
 mkdir -p $FINALCACHE
 
 # Move downloaded data back to working directory
-cp -r $TMPDB/* $FINALDB/ || fail "copying DB"
-cp -r $MNTREPO/* $FINALCACHE/ || fail "copying cache"
+cp -r $DB/* $FINALDB/ || fail "copying DB"
+cp -r $REPO/* $FINALCACHE/ || fail "copying cache"
 
 # Cleanup
 echo "[*] Cleaning up temporary files..."
-rm -rf $TMPDB
-rm -rf $MNTREPO
+rm -rf $DB
+rm -rf $REPO
 
-echo "[✓] All packages downloaded successfully and moved to working directory."
+echo "[✓] All packages downloaded successfully and cached at 'easy-arch-packages-cache/'."
 
